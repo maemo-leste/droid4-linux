4f036408f2eb2bf7e7414df6190b17332b853c80
Subhajit Paul <a0132170@ti.com>
2020-08-17 17:06:31 +0300

    GLSDK BACKPORT: km: close GEM vm using DRM API.
    Backport of commit d969679e86d0e8971108ca5b0c70df6592d755df from
    omap5-sgx-ddk-linux.
    
    It is cleaner to use drm API drm_vm_close as it serves
    both the purposes of unreferencing and closing the vm_area
    for this GEM object.
    
    Change-Id: I0dd1d16877d6c911fad47ff2317581a59a9ba8a6
    Signed-off-by: Subhajit Paul <a0132170@ti.com>
    [tony@atomide.com: updated to apply]
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/gpu/drm/pvrsgx/1.9.2253347/services4/srvkm/env/linux/mmap.c
+++ drivers/gpu/drm/pvrsgx/1.9.2253347/services4/srvkm/env/linux/mmap.c
@@ -1062,8 +1062,6 @@ MMapVOpen(struct vm_area_struct* ps_vma)
 static IMG_VOID
 MMapVCloseNoLock(struct vm_area_struct* ps_vma, PKV_OFFSET_STRUCT psOffsetStruct)
 {
-    struct drm_gem_object *obj;
-
     WARN_ON(!psOffsetStruct);
     if (!psOffsetStruct) {
         return;
@@ -1090,11 +1088,6 @@ MMapVCloseNoLock(struct vm_area_struct* ps_vma, PKV_OFFSET_STRUCT psOffsetStruct
 
 	DestroyOffsetStruct(psOffsetStruct);
     }
-
-    obj = ps_vma->vm_private_data;
-    drm_gem_object_put_unlocked(obj);
-
-    ps_vma->vm_private_data = NULL;
 }
 
 /*
@@ -1397,6 +1390,10 @@ MMapVCloseExt(struct vm_area_struct* ps_vma)
 		return;
 	LinuxLockMutexNested(&g_sMMapMutex, PVRSRV_LOCK_CLASS_MMAP);
 	MMapVCloseNoLock(ps_vma, psOffsetStruct);
+
+	drm_gem_vm_close(ps_vma);
+	ps_vma->vm_private_data = NULL;
+
 	LinuxUnLockMutex(&g_sMMapMutex);
 }
 
