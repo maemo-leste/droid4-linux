4bf4734f68aa259c7b43d1cce3d91416baaa139f
Tony Lindgren <tony@atomide.com>
2020-08-24 20:15:19 +0300

    power: supply: cpcap-charger: Fix flakey reboot with charger connected
    If we have a USB charger connected, reboot is flakey and often fails to
    reboot the device with the charger LED staying on.
    
    Let's fix this by implementing .shutdown.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/power/supply/cpcap-charger.c
+++ drivers/power/supply/cpcap-charger.c
@@ -885,7 +885,7 @@ static int cpcap_charger_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int cpcap_charger_remove(struct platform_device *pdev)
+static void cpcap_charger_shutdown(struct platform_device *pdev)
 {
 	struct cpcap_charger_ddata *ddata = platform_get_drvdata(pdev);
 	int error;
@@ -902,6 +902,11 @@ static int cpcap_charger_remove(struct platform_device *pdev)
 			 error);
 	cancel_delayed_work_sync(&ddata->vbus_work);
 	cancel_delayed_work_sync(&ddata->detect_work);
+}
+
+static int cpcap_charger_remove(struct platform_device *pdev)
+{
+	cpcap_charger_shutdown(pdev);
 
 	return 0;
 }
@@ -912,6 +917,7 @@ static struct platform_driver cpcap_charger_driver = {
 		.name	= "cpcap-charger",
 		.of_match_table = of_match_ptr(cpcap_charger_id_table),
 	},
+	.shutdown = cpcap_charger_shutdown,
 	.remove	= cpcap_charger_remove,
 };
 module_platform_driver(cpcap_charger_driver);
