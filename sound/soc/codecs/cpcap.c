1078ddbea9556f79c94984aa00638847d5a14758
Merlijn Wajer <merlijn@wizzup.org>
2022-07-31 12:05:21 +0200

    ASoC: cpcap: HACK: force enable more PGA nodes in voice call
    This is required for mic to function in earpiece/handset and headset
    modes (headset won't output sound from the remote party yet)
    
    The Headset Charge Pump is required for headset mode to work (with
    output to headphones, mic from phone, not headset)
    
    The ADC Left I just added because I think I saw it was necessary at some
    point (once I had a call with no audio, and that was the only reg diff.)

--- sound/soc/codecs/cpcap.c
+++ sound/soc/codecs/cpcap.c
@@ -1488,6 +1488,11 @@ static int cpcap_voice_call(struct cpcap_audio *cpcap, struct snd_soc_dai *dai,
 		snd_soc_dapm_force_enable_pin(w->dapm, "Headset Left PGA");
 		snd_soc_dapm_force_enable_pin(w->dapm, "Headset Right PGA");
 		snd_soc_dapm_force_enable_pin(w->dapm, "Earpiece PGA");
+		snd_soc_dapm_force_enable_pin(w->dapm, "Microphone 1 PGA");
+		snd_soc_dapm_force_enable_pin(w->dapm, "Microphone 2 PGA");
+		snd_soc_dapm_force_enable_pin(w->dapm, "ADC Right");
+		snd_soc_dapm_force_enable_pin(w->dapm, "ADC Left");
+		snd_soc_dapm_force_enable_pin(w->dapm, "Headset Charge Pump");
 		snd_soc_dapm_sync(w->dapm);
 	}
 	else
@@ -1503,6 +1508,11 @@ static int cpcap_voice_call(struct cpcap_audio *cpcap, struct snd_soc_dai *dai,
 		snd_soc_dapm_disable_pin(w->dapm, "Headset Left PGA");
 		snd_soc_dapm_disable_pin(w->dapm, "Headset Right PGA");
 		snd_soc_dapm_disable_pin(w->dapm, "Earpiece PGA");
+		snd_soc_dapm_disable_pin(w->dapm, "Microphone 1 PGA");
+		snd_soc_dapm_disable_pin(w->dapm, "Microphone 2 PGA");
+		snd_soc_dapm_disable_pin(w->dapm, "ADC Right");
+		snd_soc_dapm_disable_pin(w->dapm, "ADC Left");
+		snd_soc_dapm_disable_pin(w->dapm, "Headset Charge Pump");
 		snd_soc_dapm_sync(w->dapm);
 	}
 
