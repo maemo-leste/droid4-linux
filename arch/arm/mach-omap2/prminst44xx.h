d02769f8f56cc8b4b89496cb45015b3fa036fed3
Tony Lindgren <tony@atomide.com>
2023-11-14 16:59:27 +0200

    WIP: Enable pstore support for droid4
    Enable pstore support that can be used to view kernel oopses in
    /sys/fs/pstore after a watchdog reset, and can be also used for tracing.
    
    The reserved-memory is configured to overlap the area configured by the
    Android kernel so we can have the bootloader preserve it. We don't need
    to hog all the 2MB of the memory though that Android uses.
    
    To make pstore work, we need to change the warm reset timings to preserve
    DDR on watchdog reset, and reconfigure the PMIC. For standard reboot
    command, we need to use the default settings as the PMIC needs to wait
    for 4ms to do a cold reset for reboot to work.
    
    To test pstore crash reporting, ssh to the device, and do the following
    to crash the system:
    
    # echo 1 > /proc/sys/kernel/sysrq
    # echo c > /proc/sysrq-trigger
    
    Wait for the system to do a watchdog reset, and then ssh to the system
    again and do:
    
    # ls /sys/fs/pstore
    console-ramoops-0  dmesg-ramoops-0
    
    Note that the watchdog timeout should probably be configured to something
    shorter like 20 seconds to avoid having to wait a minute for system to
    reboot.
    
    REVISIT: This needs to be split into several patches, and probably should
             be done based on kernel command line only to reserve the memory
    	 and reconfigure the reset time
    
    Not-yet-signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/prminst44xx.h
+++ arch/arm/mach-omap2/prminst44xx.h
@@ -22,6 +22,9 @@ extern void omap4_prminst_write_inst_reg(u32 val, u8 part, s16 inst, u16 idx);
 extern u32 omap4_prminst_rmw_inst_reg_bits(u32 mask, u32 bits, u8 part,
 					   s16 inst, u16 idx);
 
+extern void omap4_prminst_global_reset_time(int pwrdm_cycles,
+					    int reset_cycles);
+
 extern void omap4_prminst_global_warm_sw_reset(void);
 
 extern int omap4_prminst_is_hardreset_asserted(u8 shift, u8 part, s16 inst,
