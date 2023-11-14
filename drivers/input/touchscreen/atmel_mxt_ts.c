63b78499551bd1a3d8ae6c6e2f500d1cb982026c
Tony Lindgren <tony@atomide.com>
2023-11-14 17:04:42 +0200

    Input: atmel_mxt_ts - use runtime PM autosuspend for idle config
    Let's keep runtime PM autosuspend disabled by default with value of -1 as
    otherwise the touchscreen not very responsive to clicks. The runtime PM
    autosuspend mode is still usable for being able to wake up the device
    based on touchscreen events.
    
    For example, user could configure runtime PM autosuspend value of 600 ms
    when the screen blanks, and then have the device wake up to the touch
    event.
    
    Let's only allow autosuspend if MXT_SUSPEND_T9_CTRL is not configured for
    suspend_mode as MXT_SUSPEND_T9_CTRL mode powers off the controller.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -1220,21 +1220,26 @@ update_count:
 static irqreturn_t mxt_interrupt(int irq, void *dev_id)
 {
 	struct mxt_data *data = dev_id;
+	irqreturn_t ret = IRQ_HANDLED;
 
 	if (data->in_bootloader) {
 		/* bootloader state transition completion */
 		complete(&data->bl_completion);
-		return IRQ_HANDLED;
+		goto out_done;
 	}
 
 	if (!data->object_table)
-		return IRQ_HANDLED;
+		goto out_done;
 
-	if (data->T44_address) {
-		return mxt_process_messages_t44(data);
-	} else {
-		return mxt_process_messages(data);
-	}
+	if (data->T44_address)
+		ret = mxt_process_messages_t44(data);
+	else
+		ret = mxt_process_messages(data);
+
+out_done:
+	pm_runtime_mark_last_busy(&data->client->dev);
+
+	return ret;
 }
 
 static int mxt_t6_command(struct mxt_data *data, u16 cmd_offset,
@@ -3162,6 +3167,17 @@ static int mxt_input_open(struct input_dev *input_dev)
 		return error;
 	}
 
+	/*
+	 * Prevent autoidle if MXT_SUSPEND_T9_CTRL is the default as it will
+	 * power off the controller. Balanced inmxt_input_close().
+	 */
+	if (data->suspend_mode == MXT_SUSPEND_T9_CTRL)
+		pm_runtime_get(dev);
+
+	pm_runtime_use_autosuspend(dev);
+	pm_runtime_mark_last_busy(dev);
+	pm_runtime_put_autosuspend(dev);
+
 	return 0;
 }
 
@@ -3169,7 +3185,20 @@ static void mxt_input_close(struct input_dev *input_dev)
 {
 	struct mxt_data *data = input_get_drvdata(input_dev);
 	struct device *dev = &data->client->dev;
+	int error;
 
+	/* Release extra usage count for MXT_SUSPEND_T9_CTRL done in open */
+	if (data->suspend_mode == MXT_SUSPEND_T9_CTRL)
+		pm_runtime_put(dev);
+
+	/* Wake the device so autosuspend sees correct input_dev->users */
+	error = pm_runtime_get_sync(dev);
+	if (error < 0) {
+		pm_runtime_put_noidle(dev);
+		return;
+	}
+
+	pm_runtime_dont_use_autosuspend(dev);
 	pm_runtime_put_sync(dev);
 }
 
@@ -3389,10 +3418,20 @@ static int mxt_probe(struct i2c_client *client)
 				 &data->wakeup_method);
 
 	pm_runtime_enable(dev);
+	/* Autosuspend won't be enabled until in open */
+	pm_runtime_dont_use_autosuspend(dev);
+	pm_runtime_set_autosuspend_delay(dev, -1);
+	error = pm_runtime_get_sync(dev);
+	if (error < 0) {
+		pm_runtime_put_noidle(dev);
+		goto err_disable_pm_runtime;
+	}
 
 	error = mxt_initialize(data);
 	if (error)
 		goto err_disable_pm_runtime;
+
+	pm_runtime_put_sync(dev);
 
 	return 0;
 
@@ -3411,6 +3450,7 @@ static void mxt_remove(struct i2c_client *client)
 	disable_irq(data->irq);
 	mxt_free_input_device(data);
 	mxt_free_object_table(data);
+	pm_runtime_dont_use_autosuspend(dev);
 	pm_runtime_disable(dev);
 	regulator_bulk_disable(ARRAY_SIZE(data->regulators),
 			       data->regulators);
